2496b0c0707236ebe97f461ab5400131e9792b0e
ksudra <135264148+ksudra@users.noreply.github.com>
2024-05-19 13:27:16 +0100

    Update PlusMinus.java

--- PlusMinus.java
+++ PlusMinus.java
@@ -12,12 +12,6 @@ import static java.util.stream.Collectors.toList;
 
 class Result {
 
-    /*
-     * Complete the 'plusMinus' function below.
-     *
-     * The function accepts INTEGER_ARRAY arr as parameter.
-     */
-
     public static void plusMinus(List<Integer> arr) {
         float p=0;
         float n=0;
